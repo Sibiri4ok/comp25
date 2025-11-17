080534e6d51100bac2b922b1de6787e6e453b1c8
Danil Usoltsev <danilusoltsev05@gmail.com>
2025-11-18 00:26:33 +0300

    fix ocamlformat

--- EML/lib/utils/monads.ml
+++ EML/lib/utils/monads.ml
@@ -1,21 +1,18 @@
-
-
 module ANFMonad = struct
   type 'a t = int -> int * ('a, string) Result.t
 
-  let return x = fun counter -> (counter, Ok x)
-  
-  let ( >>= ) m f = fun counter ->
+  let return x = fun counter -> counter, Ok x
+
+  let ( >>= ) m f =
+    fun counter ->
     match m counter with
-    | (counter', Ok a) -> f a counter'
-    | (counter', Error e) -> (counter', Error e)
+    | counter', Ok a -> f a counter'
+    | counter', Error e -> counter', Error e
+  ;;
 
-  let fresh : string t = fun counter ->
-    (counter + 1, Ok ("anf_t" ^ Int.to_string counter))
-
+  let fresh : string t = fun counter -> counter + 1, Ok ("anf_t" ^ Int.to_string counter)
   let run m = m 0 |> snd
-
-  let fail msg = fun counter -> (counter, Error msg)
+  let fail msg = fun counter -> counter, Error msg
 
   (** Монадические операторы *)
   module Syntax = struct
